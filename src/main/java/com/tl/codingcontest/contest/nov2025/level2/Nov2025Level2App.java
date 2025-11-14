662ad87d75904cdcd7cf04652a0bc6bc0142317c
Tadeusz Luczyna <tadeusz.luczyna.ext@tipico.com>
2025-11-14 15:08:17 +0100

    level2 files

---
+++ src/main/java/com/tl/codingcontest/contest/nov2025/level2/Nov2025Level2App.java
@@ -0,0 +1,34 @@
+package com.tl.codingcontest.contest.nov2025.level2;
+
+import com.tl.utils.CodingContestFileHelper;
+import com.tl.utils.ParserHelper;
+import com.tl.utils.StringIteration;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nov2025Level2App {
+
+    public static void main(String[] args) {
+        CodingContestFileHelper codingContestFileHelper = new CodingContestFileHelper(Nov2025Level2App.class);
+        List<Path> inputFilesPaths = codingContestFileHelper.readInputFiles(false);
+
+        for (var inputPath : inputFilesPaths) {
+
+            var stringIteration = new StringIteration(CodingContestFileHelper.readFile(inputPath));
+
+            int sequencesNumber = ParserHelper.getDoubles(stringIteration.getNext()).getFirst().intValue();
+
+            List<String> outputLines = new ArrayList<>();
+
+            for (int i = 0; i < sequencesNumber; i++) {
+                List<Integer> paces = ParserHelper.getInts(stringIteration.getNext());
+                int sum = paces.stream().mapToInt(x -> x).sum();
+                outputLines.add(String.valueOf(sum));
+
+            }
+            CodingContestFileHelper.saveOutputFile(inputPath, outputLines);
+        }
+        System.exit(0);
+    }
+}
