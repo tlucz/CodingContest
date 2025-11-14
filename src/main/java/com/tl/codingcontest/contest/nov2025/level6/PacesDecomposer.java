8ddf6f577cb5d09daa06a201cf6525514718d2df
alan <tadeusz.luczyna@gmail.com>
2025-11-14 18:18:31 +0100

    Level 6 Pace decomposer

--- src/main/java/com/tl/codingcontest/contest/nov2025/level6/PacesDecomposer.java
+++ src/main/java/com/tl/codingcontest/contest/nov2025/level6/PacesDecomposer.java
@@ -1,10 +1,12 @@
 package com.tl.codingcontest.contest.nov2025.level6;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PacesDecomposer {
 
     public List<Point> decompose(List<Pace> paces){
 
+        return Collections.emptyList();
     }
 }
